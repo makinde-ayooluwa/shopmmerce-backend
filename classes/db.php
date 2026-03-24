c3bfbbfb86d9dccda74026fda39855b719efbd04
Makinde Ayooluwa <makindeayooluwa604@gmail.com>
2026-03-24 12:57:21 +0100

    dockerfile changes

--- classes/db.php
+++ classes/db.php
@@ -1,8 +1,8 @@
 <?php
 
-// $pdo = new PDO(
-//     "mysql:host=" . getenv('DB_HOST') . ";dbname=" . getenv('DB_NAME'),
-//     getenv('DB_USER'),
-//     getenv('DB_PASS')
-// );
-$pdo = new PDO("mysql:host=localhost;dbname=shopmmerce","root","");
+$pdo = new PDO(
+    "mysql:host=mysql.railway.internal;dbname=railway",
+    "root",
+    "nxNWSIbJrqcVJvxyqNDoQTMUvOefbIQQ"
+);
+// $pdo = new PDO("mysql:host=localhost;dbname=shopmmerce","root","");
