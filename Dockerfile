5315b6d40707f4edb5eaf12abaf5973b364f85fc
Makinde Ayooluwa <makindeayooluwa604@gmail.com>
2026-03-24 13:09:00 +0100

    dockerfile changes

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM php:8.2-apache
 RUN docker-php-ext-install pdo pdo_mysql
 
 COPY . /var/www/html/
+WORKDIR /var/www/html/
 
 RUN a2enmod rewrite
 
--- classes/db.php
+++ classes/db.php
@@ -1,8 +1,12 @@
 <?php
-
+$host = getenv('MYSQL_HOST');
+$user = getenv('MYSQL_USER');
+$pass = getenv('MYSQL_PASSWORD');
+$db   = getenv('MYSQL_DATABASE');
+$port = getenv('MYSQL_PORT');
 $pdo = new PDO(
-    "mysql:host=mysql.railway.internal;dbname=railway",
-    "root",
-    "nxNWSIbJrqcVJvxyqNDoQTMUvOefbIQQ"
+    "mysql:host=" . $host . ";port=". $port ."dbname=" . $db,
+    $user,
+    $pass
 );
 // $pdo = new PDO("mysql:host=localhost;dbname=shopmmerce","root","");
