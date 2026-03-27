1b7717ee7115b287d0f88983346f6dd0b3f95f03
Makinde Ayooluwa <makindeayooluwa604@gmail.com>
2026-03-27 16:08:56 +0100

    new changes

--- api/db.php
+++ api/db.php
@@ -4,9 +4,16 @@ $user = getenv('MYSQLUSER');
 $pass = getenv('MYSQLPASSWORD');
 $db   = getenv('MYSQLDATABASE');
 $port = getenv('MYSQLPORT');
-$pdo = new PDO(
-    "mysql:host=" . $host . ";port=". $port ."dbname=" . $db,
-    $user,
-    $pass
-);
+
+try {
+    $pdo = new PDO(
+        "mysql:host=$host;port=$port;dbname=$db;charset=utf8",
+        $user,
+        $pass
+    );
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully!";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
 // $pdo = new PDO("mysql:host=localhost;dbname=shopmmerce","root","");
