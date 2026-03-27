fcf0ac533926ee8c809543db98e4635dc75c0f50
Makinde Ayooluwa <makindeayooluwa604@gmail.com>
2026-03-27 16:04:44 +0100

    new    changes

--- api/db.php
+++ api/db.php
@@ -4,9 +4,9 @@ $user = getenv('MYSQLUSER');
 $pass = getenv('MYSQLPASSWORD');
 $db   = getenv('MYSQLDATABASE');
 $port = getenv('MYSQLPORT');
-// $pdo = new PDO(
-//     "mysql:host=" . $host . ";port=". $port ."dbname=" . $db,
-//     $user,
-//     $pass
-// );
-$pdo = new PDO("mysql:host=localhost;dbname=shopmmerce","root","");
+$pdo = new PDO(
+    "mysql:host=" . $host . ";port=". $port ."dbname=" . $db,
+    $user,
+    $pass
+);
+// $pdo = new PDO("mysql:host=localhost;dbname=shopmmerce","root","");
